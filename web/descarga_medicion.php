c62402e39ae1f830456870b810ec76542c767df1
Luz Barcena <mluzbarcena@gmail.com>
2018-06-22 00:01:20 -0300

    grafico de ultimas mediciones con select de eleccion de horas previos funcionando

---
+++ web/descarga_medicion.php
@@ -0,0 +1,25 @@
+<?php
+
+if ($_GET["hora"] != "") {
+	$pdo = new PDO ("sqlite:db.sqlite");
+	$ahora = time();
+	#echo $ahora;
+	$date = new DateTime();
+	$date -> setTimestamp($ahora);
+	#echo $date->format('U = Y-m-d H:i:s') . "\n";
+	$desde = $ahora  - (intval($_GET["hora"]) * 60 * 60);
+	$query =
+		"SELECT temperatura, humedad, luminosidad, timestamp"
+		. " FROM medicion WHERE timestamp > :desde ORDER BY timestamp";
+
+	#$stmt = $pdo-> query($query);
+	$stmt = $pdo-> prepare($query);
+	$stmt-> execute([":desde"=>$desde]);
+	$datos_enviar = $stmt-> fetchAll(PDO::FETCH_ASSOC);
+	$file = fopen("mediciones.csv","w");
+	$headers = array_keys($datos_enviar[0]);
+	fputcsv($file, $headers);
+	foreach($datos_enviar as $key=>$value){
+		fputcsv($file, $value);
+	}	
+}
--- web/js/estacion.js
+++ web/js/estacion.js
@@ -1,3 +1,6 @@
+var datos;
+var lineChart;
+
 function actualizar_medicion () {
 	$.ajax ("medicion_actual.php",
 		{
@@ -27,7 +30,8 @@ function on_actualizar_medicion_success (data) {
 	var time_fecha = document.getElementById ("fecha");
 	var time_hora = document.getElementById ("hora");
 
-	var fecha_formateada = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+	var mes = date.getMonth() + 1;
+	var fecha_formateada = date.getDate() + "/" + mes + "/" + date.getFullYear();
 	var hora_formateada = date.getHours() + ":" + date.getMinutes();
 
 	time_fecha.textContent = fecha_formateada;
@@ -90,39 +94,179 @@ function actualizar_mapa (lat, lng) {
 	}
 }
 
-function graficar () {
-	var ctx = document.getElementById('grafico').getContext('2d');
-new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-    datasets: [{ 
-        data: [86,114,106,106,107,111,133,221,783,2478],
-        label: "Datos 1",
-        borderColor: "#3e95cd",
-        fill: false
-      }, { 
-        data: [282,350,411,502,635,809,947,1402,3700,5267],
-        label: "Datos 2",
-        borderColor: "#8e5ea2",
-        fill: false
-      }
-    ]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'Grafico de ejemplo'
-    }
-  }
-});
 
-
+function get_mediciones(hora) {
+	$.ajax ("xh_anteriores.php",
+		{
+			method: "GET",
+			dataType: "json",
+			data: {
+					"hora": hora
+				},
+			success: actualizar_data
+		}
+	)	
 }
 
+
+function actualizar_grafico() {
+	$.ajax ("medicion_actual.php",
+		{
+			method: "GET",
+			dataType: "json",
+			success: on_actualizar_grafico_success
+		}
+	)	
+}
+
+
+function on_actualizar_grafico_success(data) {
+	datos.shift(1);
+	datos.push(data)
+	actualizar_data(datos);
+}
+
+
+function actualizar_data(data) {
+	lineChart.data = getData(data);
+	lineChart.update();
+}
+
+
+function getChartOptions() {
+	var options = {
+        responsive: true,
+        stacked: false,
+        title: {
+            display: true,
+            text: 'Humedad vs Temperatura vs Luminosidad'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        scales: {
+			yAxes: [{
+				type: 'linear',
+				display: true,
+				position: 'left',
+				id: 'temperatura',
+				ticks: {
+	                suggestedMin: -10,
+	                suggestedMax: 50
+	            },
+			}, {
+				type: 'linear',
+				display: true,
+				position: 'left',
+				id: 'humedad',
+				ticks: {
+	                suggestedMin: 0,
+	                suggestedMax: 100
+	            },
+			}, {
+				type: 'linear',
+				display: true,
+				position: 'right',
+				id: 'luminosidad',
+				ticks: {
+	                suggestedMin: 0,
+	                suggestedMax: 100
+	            },
+			}],
+		}
+    };
+    return options;
+}
+
+function getData(datos) {
+	var data = {
+        labels: datos.map(function(item){
+            //return new Date(parseInt (item.timestamp*1000)).getHours();
+            var fecha_completa = new Date(parseInt (item.timestamp*1000));
+            var anio = fecha_completa.getFullYear();
+            var mes = fecha_completa.getMonth() + 1;
+            var dia = fecha_completa.getDate();
+            var hora = fecha_completa.getHours();
+            var minutos = fecha_completa.getMinutes();
+            return (dia+'/'+mes+'/'+anio+' '+hora+':'+minutos)
+        }), 
+        datasets: [{
+            label: 'Temperatura',
+            backgroundColor: 'rgba(255,2,0,0.7)',
+            borderColor: 'rgba(255,2,0,0.7)',
+            data: datos.map(function(item){
+                return item["temperatura"];
+            }),
+            fill: false,
+            pointRadius: 0,
+            yAxisID: 'temperatura',
+        }, {
+            label: 'Humedad',
+            fill: false,
+            pointRadius: 0,
+            backgroundColor: 'rgba(0,0,255,0.7)',
+            borderColor: 'rgba(0,0,255,0.7)',
+            data: datos.map(function(item){
+                return item["humedad"];
+            }),
+            yAxisID: 'humedad',
+        }, {
+            label: 'Luminosidad',
+            fill: false,
+            pointRadius: 0,
+            backgroundColor: 'rgba(0,0,0,0.7)',
+            borderColor: 'rgba(0,0,0,0.7)',
+            data: datos.map(function(item){
+                return item["luminosidad"];
+            }),
+            yAxisID: 'luminosidad',
+        }]
+    };
+
+    return data;
+}
+
+
+function agregar_evento_select_grafico() {
+	var select_grafico = document.getElementById("select_hora_grafico");
+	select_grafico.addEventListener("change", function() {
+		switch (select_grafico.value) {
+			case "24hs":
+				get_mediciones(24); 
+				break;
+			case "12hs":
+				get_mediciones(12); 
+				break;
+			case "1hs":
+				get_mediciones(1); 
+				break;
+			default:
+
+		}
+	});
+}
+
+
 window.onload = function () {
+	//inicializo el select con materialize
+	var elems = document.querySelectorAll('select');
+	var instances = M.FormSelect.init(elems);
+	//event de select para grafico
+	agregar_evento_select_grafico();
+
 	inicializar_mapa ();
 	actualizar_medicion ();
-	graficar ();
-	window.setInterval (actualizar_medicion, 3000);
+	var ctx = document.getElementById('grafico').getContext('2d');	
+	lineChart = new Chart(ctx, {
+		type: 'line',
+		options: getChartOptions()
+	});
+	// por defecto se muestran 24 horas anteriores
+	get_mediciones(24);
+	
+	// Las mediciones se envían cada 60 segundos 
+	// entonces se actualiza cada ese lapso de tiempo
+	window.setInterval (actualizar_medicion, 60000); 
+	window.setInterval (actualizar_grafico, 60000); 
 }
