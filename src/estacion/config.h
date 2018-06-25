02397bb7e1aa858c9252c980cea03237879e8c22
Luz Barcena <mluzbarcena@gmail.com>
2018-06-24 21:47:21 -0300

    nombre y clave

--- src/estacion/config.h
+++ src/estacion/config.h
@@ -1,5 +1,5 @@
-#define WIFI_SSID "BARCENA"
-#define WIFI_PASSWORD "monteolimpo2919"
+#define WIFI_SSID "nombre"
+#define WIFI_PASSWORD "clave"
 #define SERVER_HOST "pcabrera.xyz"
 #define SERVER_PORT 80
 #define SERVER_RESOURCE "/adi/cargar_datos.php"
